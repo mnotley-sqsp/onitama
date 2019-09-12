15eb48460fb6d16b9c7ee64720416d975d2baaaa
Mike N <mnotley@squarespace.com>
2019-09-12 15:40:52 -0400

    Update to start_game.js, cards.js, and turns.js.

--- cards.js
+++ cards.js
@@ -7,139 +7,141 @@ class CardList {
 
         this.cardMoves = {
 
-            'crab' : moveCrab(),
-            'eel' : moveEel(),
-            'rabbit' : moveRabbit(),
-            'crane' : moveCrane(),
-            'goose' : moveGoose(),
-            'ox' : moveOx(),
-            'monkey' : moveMonkey(),
-            'tiger' : moveTiger(),
-            'rooster' : moveRooster(),
-            'frog' : moveFrog(),
-            'mantist' : moveMantis(),
-            'horse' : moveHorse(),
-            'elephant' : moveElephant(),
-            'dragon' : moveDragon(),
-            'cobra' : moveCobra(),
-            'boar' : moveBoar(),
-            'bear' : moveBear(),
-            'panda' : movePanda(),
-            'otter' : moveOtter(),
-            'sable' : moveSable(),
-            'dog' : moveDog(),
-            'fox' : moveFox(),
-            'mouse' : moveMouse(),
-            'rat' : moveRat(),
-            'tanuki' : moveTanuki(),
-            'iguana' : moveIguana(),
-            'seasnake' : moveSeaSnake(),
-            'viper' : moveViper(),
-            'giraffe' : moveGiraffe(),
-            'kirin' : moveKirin(),
-            'phoenix' : movePhoenix(),
-            'turtle' : moveTurtle(),
+            'crab' : this.moveCrab(),
+            'eel' : this.moveEel(),
+            'rabbit' : this.moveRabbit(),
+            'crane' : this.moveCrane(),
+            'goose' : this.moveGoose(),
+            'ox' : this.moveOx(),
+            'monkey' : this.moveMonkey(),
+            'tiger' : this.moveTiger(),
+            'rooster' : this.moveRooster(),
+            'frog' : this.moveFrog(),
+            'mantist' : this.moveMantis(),
+            'horse' : this.moveHorse(),
+            'elephant' : this.moveElephant(),
+            'dragon' : this.moveDragon(),
+            'cobra' : this.moveCobra(),
+            'boar' : this.moveBoar(),
+            'bear' : this.moveBear(),
+            'panda' : this.movePanda(),
+            'otter' : this.moveOtter(),
+            'sable' : this.moveSable(),
+            'dog' : this.moveDog(),
+            'fox' : this.moveFox(),
+            'mouse' : this.moveMouse(),
+            'rat' : this.moveRat(),
+            'tanuki' : this.moveTanuki(),
+            'iguana' : this.moveIguana(),
+            'seasnake' : this.moveSeaSnake(),
+            'viper' : this.moveViper(),
+            'giraffe' : this.moveGiraffe(),
+            'kirin' : this.moveKirin(),
+            'phoenix' : this.movePhoenix(),
+            'turtle' : this.moveTurtle()
         }
 
     }
 
-    moveCrab = function()
-    { var crab = new MovementCard(currentSquare, -5, -2, 2); };
+    moveCrab()
+    { var crab = new MovementCard(currentSquare, -5, -2, 2); }
     
-    moveEel = function()
+    moveEel()
     { var eel = new MovementCard(currentSquare, -6, 1, 4); };
     
-    moveRabbit = function()
+    moveRabbit()
     { var rabbit = new MovementCard(currentSquare, -4, 2, 4); };
     
-    moveCrane = function()
+    moveCrane()
     { var crane = new MovementCard(currentSquare, -5, 4, 6); };
     
-    moveGoose = function()
+    moveGoose()
     { var goose = new MovementCard(currentSquare, -6, -1, 1, 6); };
     
-    moveOx = function()
+    moveOx()
     { var ox = new MovementCard(currentSquare, -5, 1, 5); };
     
-    moveMonkey = function()
+    moveMonkey()
     { var monkey = new MovementCard(currentSquare, -6, -4, 4, 6); };
     
-    moveTiger = function()
+    moveTiger()
     { var tiger = new MovementCard(currentSquare, -10, 5); };
     
-    moveRooster = function()
+    moveRooster()
     { var rooster = new MovementCard(currentSquare, -4, -1, 1, 4); };
     
-    moveFrog = function()
+    moveFrog()
     { var frog = new MovementCard(currentSquare, -6, -2, 6); };
     
-    moveMantis = function()
+    moveMantis()
     { var mantis = new MovementCard(currentSquare, -6, -4, 5); };
     
-    moveHorse = function()
+    moveHorse()
     { var horse = new MovementCard(currentSquare, -5, -1, 5); };
     
-    moveElephant = function()
+    moveElephant()
     { var elephant = new MovementCard(currentSquare, -6, -4, -1, 1); };
     
-    moveDragon = function()
+    moveDragon()
     { var dragon = new MovementCard(currentSquare, -7, -3, 4, 6); };
     
-    moveCobra = function()
+    moveCobra()
     { var cobra = new MovementCard(currentSquare, -4, -1, 6); };
     
-    moveBoar = function()
+    moveBoar()
     { var boar = new MovementCard(currentSquare, -5, -1, 1); };
     
     // New Cards from latest expansion
-    moveBear = function()
+    moveBear()
     { var bear = new MovementCard(currentSquare, -6, -5, 6); };
     
-    movePanda = function()
+    movePanda()
     { var panda = new MovementCard(currentSquare, -5, -4, 4); };
     
-    moveOtter = function()
+    moveOtter()
     { var otter = new MovementCard(currentSquare, -6, 2, 6); };
     
-    moveSable = function()
+    moveSable()
     { var sable = new MovementCard(currentSquare, -4, -2, 4); };
     
-    moveDog = function()
+    moveDog()
     { var dog = new MovementCard(currentSquare, -6, -1, 4); };
     
-    moveFox = function()
+    moveFox()
     { var fox = new MovementCard(currentSquare, -4, 1, 6); };
     
-    moveMouse = function()
+    moveMouse()
     { var mouse = new MovementCard(currentSquare, -5, 1, 4); };
     
-    moveRat = function()
+    moveRat()
     { var rat = new MovementCard(currentSquare, -5, -1, 6); };
     
-    moveTanuki = function()
+    moveTanuki()
     { var tanuki = new MovementCard(currentSquare, -5, -3, 4); };
     
-    moveIguana = function()
+    moveIguana()
     { var iguana = new MovementCard(currentSquare, -7, -5, 6); };
     
-    moveSeaSnake = function()
+    moveSeaSnake()
     { var seaSnake = new MovementCard(currentSquare, -5, 2, 4); };
     
-    moveViper = function()
+    moveViper()
     { var viper = new MovementCard(currentSquare, -5, -2, 6); };
     
-    moveGiraffe = function()
+    moveGiraffe()
     { var giraffe = new MovementCard(currentSquare, -7, -3, 5); };
     
-    moveKirin = function()
+    moveKirin()
     { var kirin = new MovementCard(currentSquare, -9, -11, 10); };
     
-    movePhoenix = function()
+    movePhoenix()
     { var phoenix = new MovementCard(currentSquare, -6, -4, -2, 2); };
     
     moveTurtle= function()
     { var turtle = new MovementCard(currentSquare, -2, 2, 4, 6); };
 
+
+    
 
     // Give chooseCard the card's name to pick which movement can be used.
     chooseCard(card){
@@ -157,7 +159,7 @@ class CardList {
         let randNumber = getRandomNumber(1, 32);
         this.cardNames = [
             {name:$("<div class='movementCard' id='crab'>Crab</div>")},
-            {name:$("<div class='movementCard' id='eel'>Eel</div>")}
+            {name:$("<div class='movementCard' id='eel'>Eel</div>")},
             {name:$("<div class='movementCard' id='rabbit'>Rabbit</div>")},
             {name:$("<div class='movementCard' id='crane'>Crane</div>")},
             {name:$("<div class='movementCard' id='goose'>Goose</div>")},
